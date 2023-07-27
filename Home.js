f35d8eac313cb4087414bc4e61f1c1da4c798be0
NisargaRamesh <120173050+NisargaRamesh@users.noreply.github.com>
2023-07-27 13:14:32 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <h1>Home page</h1>
+        </>,
+        <>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2ZpbghUDoVthj4OB1Rg3oisFYXbDUxfdJOw&usqp=CAU" height="600" class="d-block w-100" alt="..."></img>
+        </>
+            
+    )
+};
+
+export default Home;
